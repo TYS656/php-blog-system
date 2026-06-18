b0699f130dda32702323ab0ba041e69a03acdaba
ysyx <246777270@qq.com>
2026-06-18 16:08:07 +0800

    数据库配置文件

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 数据库配置示例
+ * 填写真实数据库信息
+ */
+return [
+    'host'     => '127.0.0.1',
+    'dbname'   => 'blog',
+    'username' => 'root',
+    'password' => '',
+    'charset'  => 'utf8mb4'
+];
